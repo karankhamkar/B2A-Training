8045e8c879ad5b2d7cdf8d456bbc7ca40097683d
karankhamkar <93980611+karankhamkar@users.noreply.github.com>
2022-12-28 23:44:08 +0530

    Feat: Add Features of WPF Commands

---
+++ Task 2/cSahrpTask/StudentManagement/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentManagement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       // ObservableCollection<Student> stuedents;
+        
+        public MainWindow()
+        {
+            //students = new ObservableCollection<Student>();
+
+            InitializeComponent();
+           // GenerateDummyStudent();
+            //dgStudent.ItemsSource = students;
+        }
+        private void Add_Click(Object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelet_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Task 2/cSahrpTask/StudentManagement/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement
+{
+    public class Student
+    {
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public char Gender { get; set; }
+        public string Address { get; set; }
+
+    }
+}
