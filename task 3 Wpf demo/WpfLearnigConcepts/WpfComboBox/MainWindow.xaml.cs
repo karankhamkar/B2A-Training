784d2689f5da99d6bae91c06f9e0503844312d50
karankhamkar <93980611+karankhamkar@users.noreply.github.com>
2023-01-02 15:57:17 +0530

    feat: Colour Combobox Implementation.

---
+++ task 3 Wpf demo/WpfLearnigConcepts/WpfComboBox/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfComboBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+		public MainWindow()
+		{
+			InitializeComponent();
+			cmbColors.ItemsSource = typeof(Colors).GetProperties();
+		}
+
+		private void btnPrevious_Click(object sender, RoutedEventArgs e)
+		{
+			if (cmbColors.SelectedIndex > 0)
+				cmbColors.SelectedIndex = cmbColors.SelectedIndex - 1;
+		}
+
+		private void btnNext_Click(object sender, RoutedEventArgs e)
+		{
+			if (cmbColors.SelectedIndex < cmbColors.Items.Count - 1)
+				cmbColors.SelectedIndex = cmbColors.SelectedIndex + 1;
+		}
+
+		private void btnBlue_Click(object sender, RoutedEventArgs e)
+		{
+			cmbColors.SelectedItem = typeof(Colors).GetProperty("Blue");
+		}
+
+		private void cmbColors_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			Color selectedColor = (Color)(cmbColors.SelectedItem as PropertyInfo).GetValue(null, null);
+			this.Background = new SolidColorBrush(selectedColor);
+		}
+	}
+}
