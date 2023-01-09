f4143fe433ac431718c48b76d93871e2388fcaa2
karankhamkar <93980611+karankhamkar@users.noreply.github.com>
2023-01-09 22:11:06 +0530

    chores: Delete .vs files

--- Wpf Projects/WpfLearnigConcepts/ListOfSuppliers/obj/Debug/net6.0-windows/MainView.g.i.cs
+++ Wpf Projects/WpfLearnigConcepts/ListOfSuppliers/obj/Debug/net6.0-windows/MainView.g.i.cs
@@ -53,7 +53,7 @@ namespace ListOfSuppliers {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ListOfSuppliers;component/mainview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ListOfSuppliers;V1.0.0.0;component/mainview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Wpf Projects/WpfLearnigConcepts/ListOfSuppliers/obj/Debug/net6.0-windows/View/SupplierItemView.g.i.cs
+++ Wpf Projects/WpfLearnigConcepts/ListOfSuppliers/obj/Debug/net6.0-windows/View/SupplierItemView.g.i.cs
@@ -53,7 +53,7 @@ namespace ListOfSuppliers.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ListOfSuppliers;component/view/supplieritemview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ListOfSuppliers;V1.0.0.0;component/view/supplieritemview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\SupplierItemView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
